48a8751d51104d880634e6c09013b9792bae4161
gvergnaud <gabriel.vergnaud@hetic.net>
2016-10-10 15:00:52 -0400

    use heroku server

--- src/services/Socket.js
+++ src/services/Socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import {Observable} from 'rxjs'
 
-const socket = io('http://localhost:3000')
+const socket = io('https://cl1pboard.herokuapp.com')
 
 export const copy$ = new Observable(observer => {
   socket.on('copy', data => {
